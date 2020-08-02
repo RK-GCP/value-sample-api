381e6bd24ceee0477011e65a2eb46e17861197c6
Rashed Kamal <rashed.kamal@ncr.com>
2020-08-01 23:06:23 -0400

    helm chart, configmap, eValues controller

---
+++ value-sample-api/Controllers/EValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace value_sample_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EValuesController : ControllerBase
+    {
+        // GET api/evalues
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {
+                $"Env Value for MY_VAL1- {System.Environment.ExpandEnvironmentVariables("%MY_VAL1%")}",
+                "value2" };
+        }
+    }
+}
